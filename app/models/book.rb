aa854f721bdaf07f3f93c3293c8a50da0bfbf02b
xdite <xuite.joke@gmail.com>
2016-11-16 20:20:52 +0800

    Add book scaffold

---
+++ app/models/book.rb
@@ -0,0 +1,13 @@
+class Book < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
---
+++ test/models/book_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class BookTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
